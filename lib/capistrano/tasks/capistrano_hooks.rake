79cdb294b34e56250a691167a2d2643386eced89
Martin Meyerhoff <mamhoff@googlemail.com>
2015-05-03 13:12:29 +0200

    load default stuff before starting

--- lib/capistrano/tasks/capistrano_hooks.rake
+++ lib/capistrano/tasks/capistrano_hooks.rake
@@ -1,5 +1,5 @@
 namespace :deploy do
-  before :check, 'uberspace:defaults'
+  before :starting, 'uberspace:defaults'
   after :published, 'uberspace:restart'
 end
 
--- lib/capistrano/uberspace.rb
+++ lib/capistrano/uberspace.rb
@@ -1,2 +1,4 @@
+require "capistrano/uberspace/version"
+
 load File.expand_path('../tasks/uberspace.rake', __FILE__)
 load File.expand_path('../tasks/capistrano_hooks.rake', __FILE__)
